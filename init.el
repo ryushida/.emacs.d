ee92ff2dfe9546d86934bf3b64c00096985a72ba
Ryuhei Shida <ryushidadev@gmail.com>
2018-04-01 16:35:30 -0700

    update migemo-dictionary directory

--- init.el
+++ init.el
@@ -239,7 +239,7 @@
   (setq migemo-options '("-q" "--emacs"))
 
   ;; Set your installed path
-  (setq migemo-dictionary "/usr/local/share/migemo/utf-8/migemo-dict")
+  (setq migemo-dictionary "/usr/share/migemo/utf-8/migemo-dict")
 
   (setq migemo-user-dictionary nil)
   (setq migemo-regex-dictionary nil)
